7dd389770e516d43176b6b07d9bb0d9ba00ab10c
albugowy15 <kholidbughowi@gmail.com>
2023-02-07 11:21:31 +0700

    feat: initial setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+import { withContentlayer } from 'next-contentlayer'
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig)
